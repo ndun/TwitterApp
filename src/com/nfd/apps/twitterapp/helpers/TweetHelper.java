d4b7bd14eeb52f20a337c5a6d46ce0e0c98ee5ef
Nicole D <nicole.dun@gmail.com>
2014-04-01 23:34:35 -0400

    added hash tag links

--- src/com/nfd/apps/twitterapp/helpers/TweetHelper.java
+++ src/com/nfd/apps/twitterapp/helpers/TweetHelper.java
@@ -23,10 +23,8 @@ public class TweetHelper {
 	}*/
 	
 	public static String formatTweetBody(JSONObject status) {
-		Tweet tweet = new Tweet(status);
-		
+		Tweet tweet = new Tweet(status);		
 		String body = tweet.getBody();
-		String htmlBody = body;
 
 		try {
 			JSONObject entities = status.getJSONObject("entities");
@@ -37,7 +35,7 @@ public class TweetHelper {
 				String displayUrl = aLink.getString("display_url");
 				String htmlLink = "<a href=\"" + expandedUrl + "\">" + displayUrl + "</a>";
 				String url = aLink.getString("url");
-				body = body.replace(url, htmlLink); //body.
+				body = body.replace(url, htmlLink); 
 			}
 			JSONArray mentions = entities.getJSONArray("user_mentions");
 			for(int i = 0; i < mentions.length(); i++) {
@@ -45,6 +43,15 @@ public class TweetHelper {
 				String screenName = aMention.getString("screen_name");
 				String linkToUser = "<a href=\"http://www.twitter.com/" + screenName + "\">@" + screenName + "</a>"; 
 				body = body.replace("@"+screenName, linkToUser);
+			}
+			
+			JSONArray hashTags = entities.getJSONArray("hashtags");
+			for(int i = 0; i < hashTags.length(); i++) {
+				JSONObject tag = hashTags.getJSONObject(i);
+				String value = tag.getString("text");
+				String searchLink = "<a href=\"https://twitter.com/search?q=%23" + value + "\">#" + value + "</a>"; 
+//				https://twitter.com/search?q=%23test
+				body = body.replace("#" + value, searchLink);
 			}
 			
 		} catch (JSONException je) {
